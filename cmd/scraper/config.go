5b0d2df94b718f5605b4d1e66d046c8b45169f35
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-03-24 22:55:02 +0530

    added configuration handling

---
+++ cmd/scraper/config.go
@@ -0,0 +1,43 @@
+package scraper
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"os"
+)
+
+type Config struct {
+	URL struct {
+		Hot100               string `yaml:"hot_100"`
+		Billboard200         string `yaml:"billboard_200"`
+		Billboard200Global   string `yaml:"billboard_200_global"`
+		BillboardJapanHot100 string `yaml:"'billboard_japan_hot_100"`
+	} `yaml:"url"`
+}
+
+var CFG Config
+
+func readConfiguration() error {
+	configFile, err := os.Open("../../configs/web-scrape-config.yaml")
+	if err != nil {
+		return fmt.Errorf("error while opening the config file: %v", err)
+	}
+	defer func(configFile *os.File) {
+		err := configFile.Close()
+		if err != nil {
+
+		}
+	}(configFile)
+
+	decoder := yaml.NewDecoder(configFile)
+	err = decoder.Decode(&CFG)
+	if err != nil {
+		return fmt.Errorf("error while decoding the contents of config file: %v", err)
+	}
+
+	return nil
+}
+
+func ValidateOAuthConfig() error {
+	return nil
+}
